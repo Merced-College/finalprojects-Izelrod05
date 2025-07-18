e12603d0c3e28eb04b751e67b5340c93d77043a8
Izelrod05 <irodriguezdiaz@ucmerced.edu>
2025-07-18 22:17:55 +0000

    finished genre recomend class using recursion

--- GenreRecommend.java
+++ GenreRecommend.java
@@ -7,13 +7,13 @@ public class GenreRecommend{
         recommendRecursive(songs, genre, count, 0);
     }
     private static void recommendRecursive(List<Song> songs, String genre, int count, int index){
-        
-
+        if(count == 0 || index >= songs.size())return;
+        Song current = songs.get(index);
+        if (current.getGenre().equalsIgnoreCase(genre)) {
+            System.out.println(current);
+            recommendRecursive(songs, genre, count -1, index+1);
+        }else{
+            recommendRecursive(songs, genre, count, index+1);
+        }
     }
-
-
-
-
-
-
 }
