36e307841bdae35bfde0a0e69c7e61b72fdda090
Izelrod05 <irodriguezdiaz@ucmerced.edu>
2025-07-18 21:48:36 +0000

    finished implementing player class

--- Player.java
+++ Player.java
@@ -4,4 +4,25 @@ public class Player{
     private Stack<Song> history = new Stack<>();
     private Queue<Song> queue = new LinkedList<>();
 
+    public void addToQueue(Song song){
+        queue.add(song);
+    }
+
+    public void playNext(){
+        if(!queue.isEmpty()){
+            Song next =queue.poll();
+            history.push(next);
+            System.out.println("Playing: " + next);
+        }else{
+            System.out.println("Queue is empty.");
+
+        }
+    }
+    public void showHistory() {
+        System.out.println("History:");
+        for (Song song : history) {
+            System.out.println(song);
+        }
+    }
+
 }
