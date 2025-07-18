4cd304710a3dc85d4ae70374bcb88407274243ed
Izelrod05 <irodriguezdiaz@ucmerced.edu>
2025-07-18 22:10:21 +0000

    started recrursive fnc for genre recommendations

--- GenreRecommend.java
+++ GenreRecommend.java
@@ -1,10 +1,19 @@
 import java.util.*;
 public class GenreRecommend{
 
+    public static void recommend(List<Song> songs, String genre, int count){
+
+        System.out.println("Recomendations for genre: " + genre);
+        recommendRecursive(songs, genre, count, 0);
+    }
+    private static void recommendRecursive(List<Song> songs, String genre, int count, int index){
+        
+
+    }
 
 
 
 
 
-    
+
 }
