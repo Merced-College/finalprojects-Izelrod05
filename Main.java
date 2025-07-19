be7923acdf32abaa6e79f95e37ebd41a58a4507a
Izelrod05 <irodriguezdiaz@ucmerced.edu>
2025-07-19 04:37:07 +0000

    Working on Main

--- Main.java
+++ Main.java
@@ -8,3 +8,38 @@
 *and genre recomender are edited and improved version of the older version along with main 
 *the other files though are completley new
 */
+
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        Library library = new Library();
+        Playlist playlist = new Playlist();
+        Player player = new Player();
+        List<Song> allSongs = new ArrayList<>();
+
+        // Add some default songs
+        Song s1 = new Song("Lose Yourself", "Eminem", "Hip-Hop");
+        Song s2 = new Song("Bohemian Rhapsody", "Queen", "Rock");
+        Song s3 = new Song("Yesterday", "The Beatles", "Pop");
+        Song s4 = new Song("HUMBLE.", "Kendrick Lamar", "Hip-Hop");
+        Song s5 = new Song("Come Together", "The Beatles", "Pop");
+
+        for(Song s:List.of(s1,s2,s3,s4,s5)){
+            library.addSong(s);
+            playlist.addSong(s);
+            player.addToQueue(s);
+            allSongs.add(s);
+        }
+
+        
+
+
+
+
+        
+
+    }
+}
